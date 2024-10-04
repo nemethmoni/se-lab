a3eb09af25ee24b6b7768e4e130ce487af898377
nemethmoni <nemethmoni03@gmail.com>
2024-10-04 13:38:44 +0200

    pull request fix

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -14,6 +14,7 @@ public class TorpedoStore {
 
   private int torpedoCount = 0;
 
+  //a túlmelegedés esélyét generálja
   private Random random = new Random();
 
   public TorpedoStore(int numberOfTorpedos){
